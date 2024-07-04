b7ec013bb3e14c2816c27c2f4bc56c016786a538
PeregrineVoid <peregrine@boum.org>
2024-07-04 19:10:01 +0200

    Aligned VBIStruct, bugged VBIHandler? DNW: Illegal instruction

--- dots.s
+++ dots.s
@@ -9,9 +9,12 @@ VBFlag              EQU     0
 ;;;;;;;;;;;;;;;;;;; END SYMBOLIC CONSTANTS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;; BEGIN MACROS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-CALLSYS             MACRO ;CALLSYS <library base>,<library routine offset>
+CALLSYS             MACRO      ;CALLSYS <library base>,<library routine offset>
 	                movea.l	\1,a6
 	                jsr		_LVO\2(a6)
+	                ENDM
+JUMPSYS             MACRO                     ;JUMPSYS <library routine offset>
+	                jsr		_LVO\1(a6)
 	                ENDM
 RESTORE_CTRL_REGS   MACRO ;RESTORE_CTRL_REGS <hardware control register>
                     move.w  #$7fff,\1(a5)                  ;disable current reg
@@ -28,10 +31,10 @@ Start:              movem.l d1-d7/a0-a6,-(sp)
 Setup:              bsr     SystemSetup
 MainLoop:           btst.b  #VBFlag,Flags
                     beq.s   MainLoop
-                    ; do stuff here
+                    ;do stuff here
                     bclr.b  #VBFlag,Flags
-                    btst    #FIR0,CIAA+CIAAPRA               ;left mouse button
-                    bne     MainLoop
+                    btst.b  #FIR0,CIAA+CIAAPRA               ;left mouse button
+                    bne.s   MainLoop
 Cleanup:            bsr     SystemCleanup
 Exit:               movem.l (sp)+,d1-d7/a0-a6
                     move.b  ErrNo,d0
@@ -39,39 +42,30 @@ Exit:               movem.l (sp)+,d1-d7/a0-a6
 ;;;;;;;;;;;;;;;;;;; END MAIN ROUTINE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;; BEGIN SYSTEM SETUP ROUTINE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-SystemSetup:        lea     CUSTOM,a5
-                    move.w  INTENAR(a5),OldINTENA
-                    move.w  INTREQR(a5),OldINTREQ
-                    move.w  #(CUSTOMCLR|INTEN),INTENA(a5)
-                    moveq   #5,d0 ;VERTB interrupt line
+SystemSetup:        moveq   #5,d0 ;VERTB interrupt line
                     lea     VBlankIntStruct,a1
                     CALLSYS SysBase,AddIntServer   ;alternative: SetIntVector()
-                    move.w  #(CUSTOMSET|INTEN|VERTB),INTENA(a5)
                     rts
 ;;;;;;;;;;;;;;;;;;; END SYSTEM SETUP ROUTINE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;; BEGIN SYSTEM CLEANUP ROUTINE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-SystemCleanup:      lea     CUSTOM,a5
-                    move.w  #(CUSTOMCLR|INTEN),INTENA(a5)
-                    moveq   #5,d0
+SystemCleanup:      moveq   #5,d0
                     lea     VBlankIntStruct,a1
                     CALLSYS SysBase,RemIntServer
-                    RESTORE_CTRL_REGS INTENA
-                    RESTORE_CTRL_REGS INTREQ
                     rts
 ;;;;;;;;;;;;;;;;;;; END SYSTEM CLEANUP ROUTINE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;; BEGIN INTERRUPT HANDLERS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
                     SECTION main.interrupts,CODE_P
                     CNOP    0,4
-VBlankIntHandler:   movem.l d1-d7/a1-a6,-(sp)            ;will return a0 and d0
-                    add.l   #1,(a1)                               ;FrameCount++
-                    bset.b  #VBFlag,4(a1)              ;set VBFlag bit of Flags
+VBlankIntHandler:   ;movem.l d1-d7/a2-a6,-(sp)            ;will return a0 and d0
+                    add.l   #1,(a1)                              ;FrameCount++
+                    bset.b  #VBFlag,4(a1)             ;set VBFlag bit of Flags
                     lea     CUSTOM,a0
                     move.w  #VERTB,INTREQ(a0)             ;clear interrupt flag
                     move.w  #VERTB,INTREQ(a0)         ;twice for compatibility?
+                    ;movem.l (sp)+,d1-d7/a2-a6
                     moveq   #0,d0
-                    movem.l (sp)+,d1-d7/a1-a6
                     rts
 ;;;;;;;;;;;;;;;;;;; END INTERRUPT HANDLERS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -90,11 +84,12 @@ OldINTREQ           ds.l    1
                     CNOP    0,4
                     ;mixed-length (structs)
                     CNOP    0,4
-VBlankIntData:      ;data shared with interrupt handler
+VBlankIntData       ;data shared with interrupt handler
 FrameCount          dc.l    0
 Flags               dc.b    0
                     ;AmigaOS' interrupt struct
-VBlankIntStruct:    ds.l    1,1                         ;LN_SUCC,LN_PRED
+                    CNOP    0,4
+VBlankIntStruct     ds.l    1,1                         ;LN_SUCC,LN_PRED
                     dc.b    2                           ;LN_TYPE = NT_INTERRUPT
                     dc.b    127                         ;LN_PRI to highest
                     dc.l    VBlankIntName
